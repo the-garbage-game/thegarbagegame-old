1e770c1dae112f0c4539e1e2f409006789f49981
Isaac Kroeker <isaackroeker@outlook.com>
2024-11-17 16:06:57 -0600

    Added navbar with bootstrap theme and fonts

---
+++ src/app/navbar/navbar.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavbarComponent } from './navbar.component';
+
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NavbarComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the navbar', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should have a title", () => {
+    let compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector(".navbar-brand")?.textContent).toContain("The Garbage Game")
+  });
+});
